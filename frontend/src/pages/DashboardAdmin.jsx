fa4e62628554877545bde4f676c0e78256d27b46
ThashmikaRathnayake <thashrath7@gmail.com>
2025-04-13 20:20:44 +0530

    feat: add addDeviceForm

--- frontend/src/pages/DashboardAdmin.jsx
+++ frontend/src/pages/DashboardAdmin.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Nav from "../components/Nav";
 import Form from "../components/Form"
 import PasswordChangeForm from "../components/PasswordChangeForm"
+import AddDeviceForm from "../components/AddDeviceForm"
 
 const DashboardAdmin = () => {
   return (
@@ -11,7 +12,7 @@ const DashboardAdmin = () => {
         <div className='flex flex-1 overflow-hidden'>
           <p>Sidebar</p>
           <main className='flex-1 p-6 overflow-auto flex justify-center items-center'>
-            <PasswordChangeForm />
+            <AddDeviceForm />
           </main>
         </div>
       </div>
