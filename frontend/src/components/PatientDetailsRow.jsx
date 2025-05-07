66fd5a759976a22fc7f7a7832ee58e97e85a2c10
ThashmikaRathnayake <thashrath7@gmail.com>
2025-05-08 01:27:55 +0530

    fix: change colours of Admitted and Discharged text

--- frontend/src/components/PatientDetailsRow.jsx
+++ frontend/src/components/PatientDetailsRow.jsx
@@ -4,11 +4,6 @@ import { useNavigate } from 'react-router-dom';
 const PatientDetailsRow = ({patientData}) => {
     const navigate = useNavigate();
 
-    // Example status - in a real application, this would be passed as a prop
-    // const status = "Admitted"; // Or "Discharged", or any other value
-    const blueTextColor = "#34A8DD";
-    const blueBgLightColor = "#E1F5FE"; // A light blue similar to Tailwind's blue-100
-
     return (
         <div className="flex items-center border-b py-3 px-4 hover:bg-gray-50 transition duration-150">
             <div className="w-1/5">{patientData.patient_name}</div>
@@ -17,9 +12,9 @@ const PatientDetailsRow = ({patientData}) => {
             <div className="w-1/5">
                 <span
                     className={`${patientData.patient_status === "Admitted"
-                            ? `bg-[${blueBgLightColor}] text-[${blueTextColor}] border-[${blueTextColor}]`
+                            ? ` text-[#34A8DD] border-[#34A8DD]`
                             : patientData.patient_status === "Discharged"
-                                ? `bg-[${blueBgLightColor}] text-[${blueTextColor}] border-[${blueTextColor}]`
+                                ? `text-[#DC2626] border-[#DC2626]`
                                 : "bg-gray-100 text-gray-600 border-gray-400"
                         } text-sm px-3 py-1 rounded border`}
                 >
