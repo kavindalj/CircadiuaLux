1723f626d628fc0b1203402b3a7ff4cd84814dfd
ThashmikaRathnayake <thashrath7@gmail.com>
2025-05-15 17:36:32 +0530

    feat: fetch and display user data in all users table

---
+++ frontend/src/components/UsersListPagination.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const UsersListPagination = ({totalUsers,UsersPerPage,setCurrentPage,currentPage}) => {
+
+    const pages = [];
+
+    for(let i=1; i<= Math.ceil(totalUsers/UsersPerPage) ; i++) {
+       pages.push(i); 
+    }
+
+    return (
+        <div className="flex justify-center mt-4 gap-2">
+            {pages.map((page) => (
+                <button
+                key={page}
+                onClick={() => setCurrentPage(page)}
+                className={`px-3 py-1 border rounded cursor-pointer ${page === currentPage ? 'bg-[#34A8DD] hover:bg-[#056c9c] text-white' : 'bg-white text-gray-800 border-[#34A8DD] hover:bg-gray-100'}`}
+                >
+                {page}
+                </button>
+            ))}
+        </div>
+  );
+};
+
+export default UsersListPagination
