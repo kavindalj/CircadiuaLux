2732c4ea67b0caf7cfa7baaee4d0613b67d42654
Kavinda L Jayarathna <kavindaljayarathna725@gmail.com>
2025-05-16 19:45:48 +0530

    feat(debug): stream sensor data to Teleplot for visualization

--- IoT/firmware/src/firmware.cpp
+++ IoT/firmware/src/firmware.cpp
@@ -1,26 +1,28 @@
 #include <Arduino.h>
 #include "config.h"       // For general pin configurations if needed here
 #include "sensor.h"       // Include sensor header
+#include "lightcontrol.h" // Include lightcontrol header
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial) {
     delay(10); // Wait for serial port to connect on native USB boards
   }
   Serial.println("Firmware Starting...");
 
   setupSensor(); // Initialize the sensor module
+  // setupCCTPins(); // Initialize the CCT pins
+
 }
 
 void loop() {
   SensorData sensorReadings = readSensorValues();
 
   if (sensorReadings.isValid) {
-    Serial.print("Color Temp: "); Serial.print(sensorReadings.colorTemp); Serial.print(" K, ");
-    Serial.print("Lux: "); Serial.print(sensorReadings.lux); Serial.println(", ");
+    Serial.print(">ColorTemp:"); Serial.println(sensorReadings.colorTemp);
+    Serial.print(">Lux:"); Serial.println(sensorReadings.lux);
   } else {
     Serial.println("Failed to read valid sensor data.");
   }
 
-  delay(1000);
 }
